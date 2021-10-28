d1f9cdb46dde2595b55dafcf60475409813ffc6b
Alexander Reit <Alexander.Reit@ebz-group.com>
2021-10-28 15:32:53 +0200

    Initial

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,72 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: ' ',
+  headline: 'Schalten, wo Sie es wollen!',
+  description:
+    'Neubau, Modernisierung oder Anbau, wir bieten zuverlässige Elektroinstallation für Ihr Gebäude.',
+  buttonLabel: 'Kontaktieren Sie uns!',
+  imgStart: '',
+  img: '',
+  alt: '',
+  start: '',
+  bg: require('../../images/wago.jpg')
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Instant Setup',
+  headline: 'Extremely quick onboarding process',
+  description:
+    "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../../images/svg-2.svg'),
+  alt: 'Vault',
+  start: '',
+  bg: ''
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Alexander Reit',
+  headline:
+    'Über mich schreiben ...',
+  description:
+    "Mehr Text hier ...",
+  buttonLabel: 'Kontaktieren Sie mich',
+  imgStart: 'start',
+  img: require('../../images/AR.png'),
+  alt: 'Foto',
+  start: 'true',
+  bg: ''
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: true,
+  lightTopLine: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Secure Database',
+  headline: 'All your data is stored on our secure server',
+  description:
+    'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true',
+  bg: ''
+};
